ba3b550fad6592a5a889f96fe8c319953455fc71
Jan Losacker <losacker.jan@gmx.de>
2021-07-29 11:35:58 +0100

    cut3 - split one string result array of one

--- tag_manipulator.py
+++ tag_manipulator.py
@@ -1,9 +1,20 @@
-class TagManipulator:
+import re
+
+class TagManipulator():
 
     def parse_string(self, tags):
         result = []
 
         if len(tags) < 1:
             return result
+
+        return result
+
+    def parse_string(self, tags, regex=""):
+        result = []
+
+        tempResult = re.split( regex, tags )
+        if( len(tempResult[0]) > 0 ):
+            result = tempResult
 
         return result
